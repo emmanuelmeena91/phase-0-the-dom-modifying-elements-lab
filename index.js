60512e2a74c0dff215ded48bb72becd5b0a6973a
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-06-29 20:07:34 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Remove the 'main#main' node
+document.querySelector('main#main').remove();
+
+// Create a new <h1> node and assign it to the 'newHeader' variable
+const newHeader = document.createElement('h1');
+
+// Set the 'id' attribute of the new <h1> node to 'victory'
+newHeader.id = 'victory';
+
+// Set the innerHTML of the new <h1> node to include the appropriate text
+newHeader.innerHTML = `${YOUR_NAME} is the champion`;
